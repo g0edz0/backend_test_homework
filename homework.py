14684931b6de861e65dcb2f279783cc65e4940fb
Maksim Sadilin <mojave@mojaves-MacBook-Pro.local>
2022-02-21 18:58:17 +0300

    homework.py added

---
+++ homework.py
@@ -0,0 +1,92 @@
+class InfoMessage:
+    """Информационное сообщение о тренировке."""
+
+    def __init__(self,
+                 training_type: str,
+                 duration: float,
+                 distance: float,
+                 speed: float,
+                 calories: float) -> None:
+        self.training_type = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
+
+    def get_message(self) -> None:
+        """Возвращает строку сообщения"""
+
+        print(f'Тип тренировки: {self.training_type}; Длительность: {self.duration} ч.; Дистанция: {self.distance} км; '
+              f'Ср. скорость: {self.speed} км/ч; Потрачено ккал: {self.calories}.')
+
+
+class Training:
+    """Базовый класс тренировки."""
+    LEN_STEP: float
+    M_IN_KM: int = 1000
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 ) -> None:
+        self.action = action
+        self.duration = duration
+        self.weight = weight
+
+    def get_distance(self) -> float:
+        """Получить дистанцию в км."""
+
+        distance = (self.action * self.LEN_STEP) / self.M_IN_KM
+        return distance
+
+    def get_mean_speed(self) -> float:
+        """Получить среднюю скорость движения."""
+
+        mean_speed = Training.get_distance(self) / self.duration
+        return mean_speed
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
+        pass
+
+    def show_training_info(self) -> InfoMessage:
+        """Вернуть информационное сообщение о выполненной тренировке."""
+        pass
+
+
+class Running(Training):
+    """Тренировка: бег."""
+    pass
+
+
+class SportsWalking(Training):
+    """Тренировка: спортивная ходьба."""
+    pass
+
+
+class Swimming(Training):
+    """Тренировка: плавание."""
+    pass
+
+
+def read_package(workout_type: str, data: list) -> Training:
+    """Прочитать данные полученные от датчиков."""
+    pass
+
+
+def main(training: Training) -> None:
+    """Главная функция."""
+    pass
+
+
+if __name__ == '__main__':
+    packages = [
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
+
+    for workout_type, data in packages:
+        training = read_package(workout_type, data)
+        main(training)
--- program.py
+++ program.py
@@ -1,1 +1,23 @@
-print('Я домашка')
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
+
+
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """Проверяет корректность введенного значения."""
+    if your_number < 0:
+        return
+
+    root = calculate_square_root(your_number)
+    print('Мы вычислили корень квадратный из введенного вами числа. '
+          'Это будет: 'f'{root}')
+
+
+print(message)
+calc(25.5)
